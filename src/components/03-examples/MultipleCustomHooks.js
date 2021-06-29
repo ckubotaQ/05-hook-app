7e91dff999007c20c25f2f90ac4b254d535bf1e1
Carlos de Jesús Kubota Pérez <ckubota@quaxar.com>
2021-06-29 18:34:38 -0500

    update hooks

--- src/components/03-examples/MultipleCustomHooks.js
+++ src/components/03-examples/MultipleCustomHooks.js
@@ -1,13 +1,48 @@
 import React from 'react'
+import { useCounter } from '../../hooks/useCounter'
 import { useFetch } from '../../hooks/useFetch'
 import "../02-useEffect/effects.css"
 export const MultipleCustomHooks = () => {
-    const state = useFetch ( `https://www.breakingbadapi.com/api/quotes/1`)
-    console.log(state)
-    
+   const {counter, increment} =useCounter(1);
+
+    const {loading, data} = useFetch ( `https://www.breakingbadapi.com/api/quotes/${counter}`)
+    //console.log(state)
+    const {author, quote} = !!data && data[0];
     return (
         <div>
-            <h1>Custom Hooks!!!</h1>
+            <h1>BreakingBad Quotes</h1>
+            <hr />
+        {
+
+            loading
+            ? 
+            
+                (
+                    <div className="alert alert-info text-center">
+                loading...
+            </div>
+                )
+            : 
+            (
+                <blockquote className="blockquote text-right">
+                <p className="mb-2">{quote}</p>
+                <footer className="blockquote-footer">
+                    {author}
+                </footer>
+                <button className="btn btn-primary" 
+                onClick={() => increment()}
+                >
+                    Siguiente quote
+                </button>
+            </blockquote>
+                
+
+            )
+        }
+
+
+            
+            
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
-import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
+//import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
+//import { FocusScreen } from './components/04-useRef/FocusScreen';
+//import { RealExampleRef } from './components/04-useRef/RealExampleRef';
+import { Layout } from './components/05-useLayoutEffect/LayoutEffect';
 // import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
 // import { SimpleForm } from './components/02-useEffect/SimpleForm';
 // import { CounterApp } from './components/01-useState/CounterApp';
@@ -9,7 +12,7 @@ import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHook
 
 ReactDOM.render(
  
-    <MultipleCustomHooks />,
+    <Layout />,
  
   document.getElementById('root')
 );
