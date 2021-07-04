6dd3aef98115d7aa493fa691c87106743fb3cdf4
Carlos de Jesús Kubota Pérez <ckubota@quaxar.com>
2021-07-04 00:53:42 -0500

    update to memos

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { MemoHook } from './components/06-memos/MemoHook';
 // import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
 //import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
 //import { FocusScreen } from './components/04-useRef/FocusScreen';
 //import { RealExampleRef } from './components/04-useRef/RealExampleRef';
-import { Layout } from './components/05-useLayoutEffect/LayoutEffect';
+//import { Layout } from './components/05-useLayoutEffect/LayoutEffect';
+//import { Memorize } from './components/06-memos/Memorize';
 // import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
 // import { SimpleForm } from './components/02-useEffect/SimpleForm';
 // import { CounterApp } from './components/01-useState/CounterApp';
@@ -12,7 +14,7 @@ import { Layout } from './components/05-useLayoutEffect/LayoutEffect';
 
 ReactDOM.render(
  
-    <Layout />,
+    <MemoHook />,
  
   document.getElementById('root')
 );
