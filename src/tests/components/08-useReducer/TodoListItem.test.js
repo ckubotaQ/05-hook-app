5a62ad2ea51c41b7e2d11ab11128c46f62a80085
Carlos de Jesús Kubota Pérez <ckubota@quaxar.com>
2021-07-13 16:45:15 -0500

    add new test TodoListItem

--- src/tests/components/08-useReducer/TodoListItem.test.js
+++ src/tests/components/08-useReducer/TodoListItem.test.js
@@ -5,33 +5,44 @@ import { demoTodos } from '../../archivosfijos/demoTodos';
 
 
 describe('Pruebas en <TodoListItem />', () => {
+    const handleToggle =jest.fn(); 
+    const handleDelete =jest.fn(); 
+    const wrapper = shallow (<
+        TodoListItem 
+        todo={demoTodos[0]}
+        index= {1}
+        handleToggle={handleToggle}
+        handleDelete={handleDelete}
+         />)
+
+
     test('debe de mostrarse correctamente', () => {
-        const wrapper = shallow (<
-            TodoListItem 
-            todo={demoTodos}
-            index= {1}
-             />)
+     expect(wrapper).toMatchSnapshot();
     })
     test('debe de llamar la funcion handledelete', () => {
-        const wrapper = shallow (<
-            TodoListItem 
-            todo={demoTodos}
-            index= {1}
-             />)
+     wrapper.find('button').simulate('click');
+     expect(handleDelete).toHaveBeenCalledWith(demoTodos[0].id);
     })
     test('debe de llamar la funcion handleToggle', () => {
-        const wrapper = shallow (<
-            TodoListItem 
-            todo={demoTodos}
-            index= {1}
-             />)
+        wrapper.find('p').simulate('click');
+     expect(handleToggle).toHaveBeenCalledWith(demoTodos[0].id);
     })
     test('debe de mostrar el texto correctamnte', () => {
-        const wrapper = shallow (<
-            TodoListItem 
-            todo={demoTodos}
-            index= {1}
-             />)
+      const p = wrapper.find('p');
+      expect(p.text()).toBe(`2. ${demoTodos[0].desc}`)
+    })
+    test('debe de tener la clase complete si el TODO.DONE esta en true', () => {
+      const todo = demoTodos[0];
+      todo.done= true;
+      const wrapper = shallow (<
+        TodoListItem 
+        todo={todo}
+         />)
+        expect(wrapper.find('p').hasClass('complete'))
+      
+    
+    
+    
     })
     
     
